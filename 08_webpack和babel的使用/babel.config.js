57e56fd33592d039d6e33572d5d000bf66f2b1cd
ccbeango <liuyh940@gmail.com>
2022-05-11 22:20:59 +0800

    webpack中使用babel

---
+++ 08_webpack和babel的使用/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    // '@babel/plugin-transform-arrow-functions',
+    // '@babel/plugin-transform-block-scoping'
+  ],
+  presets: [
+    '@babel/preset-env',
+    // ['@babel/preset-env', {
+    //   targets: "last 2 version"
+    // }],
+  ],
+}
--- 08_webpack和babel的使用/webpack.config.js
+++ 08_webpack和babel的使用/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  devtool: 'nosources-source-map',
+  devtool: 'source-map',
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
@@ -12,14 +12,35 @@ module.exports = {
   },
   module: {
     rules: [
+      // {
+      //   test: /\.js$/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       plugins: [
+      //         '@babel/plugin-transform-arrow-functions',
+      //         '@babel/plugin-transform-block-scoping'
+      //       ]
+      //     }
+      //   }
+      // },
+      // {
+      //   test: /\.js$/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: [
+      //         // '@babel/preset-env'
+      //         ['@babel/preset-env', { // 数组格式，第二个元素设置options
+      //           targets: "last 2 version"
+      //         }]
+      //       ]
+      //     }
+      //   }
+      // },
       {
         test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        loader: 'babel-loader'
       }
     ]
   },
