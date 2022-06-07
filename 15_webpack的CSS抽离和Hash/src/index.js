381ac5b756d3bac9d230f63828a8790d5155b721
ccbeango <liuyh940@gmail.com>
2022-06-07 22:01:06 +0800

    webpack的CSS抽离和Hash

---
+++ 15_webpack的CSS抽离和Hash/src/index.js
@@ -0,0 +1,8 @@
+import moment from 'moment'
+import lodash from 'lodash'
+
+console.log(lodash.join(['hello', 'webpack']))
+console.log(moment().format('YYYY-MM-DD HH:MM:SS'))
+// import('lodash').then((res) => {
+//   console.log('hello webpack from async')
+// })
