05e90cd2e28b70d49d9eb05e588c0ade1b05ea3d
ccbeango <liuyh940@gmail.com>
2022-06-12 23:11:07 +0800

    treeshaking

---
+++ 19_webpack中的TreeShaking/config/webpack.prod.js
@@ -0,0 +1,44 @@
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = merge(baseConfig, {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:6].css',
+      chunkFilename: 'css/[name].[contenthash:6].css'
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          compress: {
+            arguments: true
+          },
+          mangle: true,
+          toplevel: true,
+          keep_classnames: true,
+          keep_fnames: true
+        }
+      }),
+      new CssMinimizerPlugin()
+    ]
+  }
+})
